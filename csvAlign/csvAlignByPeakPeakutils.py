5f481e5a44b6be54ec48cad8536783ed72f5ba66
Fernando Pereira <fpereira@fedora.fp>
2016-04-19 13:53:46 -0300

    align fix

--- csvAlign/csvAlignByPeakPeakutils.py
+++ csvAlign/csvAlignByPeakPeakutils.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 import math
 import peakutils
 
+period = 2.048E-5 #s
 slc_beg = 0
 slc_end = 5000
 a,b = np.loadtxt('Comparison1.csv', delimiter=',', usecols=(0,1), unpack=True, dtype=float)
@@ -41,65 +42,27 @@ print "Mean dist:\n\ta: %f\n\tb: %f" % (mean_dist_a, mean_dist_b)
 print ind_peak_a
 print ind_peak_b
 
+if not ((mean_dist_a > 0.9*mean_dist_b) and (mean_dist_a < 1.1*mean_dist_b)):
+    print "Not suitable"
+    exit
 
+REF  = 0       # Reference setpoint
+REF_pk = 0
+LAT = 0        # Late setpoint
+LAT_pk = 0
+if mean_dist_a<= mean_dist_b:
+    REF = A
+    REF_pts = ind_peak_a
+    LAT = B
+    LAT_pk = ind_peak_b
+else:
+    REF = B
+    REF_pk = ind_peak_b
+    LAT = A
+    LAT_pk = ind_peak_a
 
+dst_t = (LAT_pk[0] - REF_pk[0])*period
 
-
-# search_space = 10E-3 #s
-# period = 2.048E-5 #s
-# desloc_lim = int(math.ceil(search_space/period))
-# search_depth = desloc_lim #Ate onde olhar para minimizacao?
-
-# start_s = desloc_lim  #Posicao para inicia pesquisa
-
-# print "\tLim Desl: %d\n\tSearch Depth %d" % (desloc_lim,search_depth)
-# print "\tSizeofB: %d" % len(B[0])
-# sq_error = []
-# for offset in range(-desloc_lim,desloc_lim):
-#     v = 0
-#     for i in range(search_depth)
-#         a = A[1][(start_s+i)]
-#         b = B[1][(start_s+offset+i)]
-#         _ = math.pow(a-b,2)
-#         if (a >= 0.16) or (b >= 0.16): 
-#             v += _
-#     sq_error.append( v )
-# #    print "Error for offset %d : %f" % (offset, v)
-
-# #err_min = min(sq_error)
-# #print sq_error
-# index_min = np.argmin(sq_error)
-# desloc_time = (index_min-desloc_lim)*period
-
-# print "Desl indice: %d  -- tempo %f" % (index_min, desloc_time)
-# #print "Erro: %f " % (err_min)
-
-
-
-# # a = [ x+desloc_time for x in B[0] ] 
-# # B = [a,B[1]]
-
-# offset = index_min - desloc_lim
-# if offset < 0:
-#     a = A[0][-offset:]
-#     b = B[1][:offset]
-#     B = [a,b]
-#     print "MENOR!"
-# else:
-#     a = A[0][:offset]
-#     b = B[1][-offset:]
-#     B = [a,b]
-#     print "Maior!"
-
-# plt.plot(A[0],A[1], 'r', B[0],B[1], 'b')
-# plt.show()
-
-# plt.plot(B[0],B[1], 'r',C[0],C[1], 'b')
-# plt.show()
-
-# #--------------------------
-
-
-
+LAT[0] = [ x - dst_t for x in LAT[0] ]
 
 
